87fb9631d69e3ca2990a4cac9a411b71a88875bc
Letourneur Léo <letourneur.leo@gmail.com>
2016-12-07 10:03:28 +0100

    Gestion des erreurs de ouf

--- index.js
+++ index.js
@@ -20,10 +20,15 @@ var list = null;  //List of shuffled questions
  * Use 'mongodb://localhost:27017/thuguizbd' for dev
  * Use '//mongodb://userepul:epul@172.31.7.31:27017/thuguizbd' for prod
  */
-MongoClient.connect('mongodb://localhost:27017/thuguizbd', (err, database) => {
-  if (err) return console.log(err)
-  db = database
-})
+function connect() {
+  MongoClient.connect('mongodb://localhost:27017/thuguizbd', (err, database) => {
+    if (err) {
+      res.render('pages/error', {err: err}); 
+      return;
+    }
+    db = database
+  })
+}
 
 /**
  * Shuffle attribut in array
@@ -40,12 +45,18 @@ function shuffle(a) {
     return a;
 }
 
+connect();
+
 /**
  * Index : Display best scores
  */
 app.get('/', (req, res) => {
-  db.collection('players').find().sort('score', 'desc').toArray((err, r_players) => {
-    if (err) res.render('pages/error', {err: err});
+
+  db.collection('players').find().sort('score', 'desc').limit(10).toArray((err, r_players) => {
+    if (err) {
+      res.render('pages/error', {err: err}); 
+      return;
+    }
 
     number = 1;
     score = 0;
@@ -60,18 +71,29 @@ app.get('/', (req, res) => {
  */
 app.post('/game', (req, res) => {
 
-  db.collection('players').find().sort('score', 'desc').toArray((err, r_players) => {
-    if (err) res.render('pages/error', {err: err});
+  db.collection('players').find().sort('score', 'desc').limit(10).toArray((err, r_players) => {
+    if (err) {
+      res.render('pages/error', {err: err}); 
+      return;
+    }
 
     //Lancement du jeu
     if(list == null) {
       number = 1;
       score = 0;
       db.collection('questions').find().toArray((err, r_questions) => {
-        if (err) res.render('pages/error', {err: err});
+        if (err) {
+          res.render('pages/error', {err: err}); 
+          return;
+        }
 
         list = shuffle(r_questions);
-          
+        
+        if(number >= list.length) {
+          res.render('pages/error', {err: "L'application ne contient pas de question."});
+          return;
+        }
+        
         renderGame(res, r_players);
       })
     } //Nouvelle question 
@@ -83,6 +105,10 @@ app.post('/game', (req, res) => {
         res.redirect('/gameover');
       }
 
+      if(number >= list.length) {
+        res.redirect('/gameover');
+      }
+      
       renderGame(res, r_players);
     }
   })
@@ -93,18 +119,22 @@ app.post('/game', (req, res) => {
  */
 function renderGame(res, r_players) {
 
-    if(list == null || number >= list.length) res.redirect('/gameover');
-
     //Get current question and current answer
     var question = list[number-1];
-    var uid = crypto.createHash('md5').update(question.reponse1+question.reponse1).digest("hex");
+    if(question != null) {
+      var uid = crypto.createHash('md5').update(question.reponse1+question.reponse1).digest("hex");
 
-    //Shuffle answers
-    var array = [ question.reponse1, question.reponse2, question.reponse3 ];
-    array = shuffle(array);
-    question.reponse1 = array[0];
-    question.reponse2 = array[1];
-    question.reponse3 = array[2];
+      //Shuffle answers
+      var array = [ question.reponse1, question.reponse2, question.reponse3 ];
+      array = shuffle(array);
+      question.reponse1 = array[0];
+      question.reponse2 = array[1];
+      question.reponse3 = array[2];
+
+    } else {
+      res.render('pages/error', {err: "Une erreur est survenue"});
+      return;
+    }
 
     res.render('pages/game', {
       players: r_players,
@@ -124,8 +154,11 @@ app.get('/gameover', (req, res) => {
   score = number;
   number = 1;
 
-  db.collection('players').find().sort('score', 'desc').toArray((err, r_players) => {
-    if (err) res.render('pages/error', {err: err});
+  db.collection('players').find().sort('score', 'desc').limit(10).toArray((err, r_players) => {
+    if (err) {
+      res.render('pages/error', {err: err}); 
+      return;
+    }
 
     res.render('pages/gameover', {
       number: score,
@@ -147,7 +180,10 @@ app.post('/retry', (req, res) => {
   var player = { pseudo : pseudo, score : score }
   
   db.collection('players').save(player, (err, result) => {
-    if (err) res.render('pages/error', {err: err});
+    if (err) {
+      res.render('pages/error', {err: err}); 
+      return;
+    }
     
     res.redirect(307, '/game');    //Redirect 307 for POST
   })
